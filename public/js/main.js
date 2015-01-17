f3e03507a985b4587fdf37377330df9b3f0ebd0b
Jonas Vogel <jonas@jonasvogel.name>
2015-01-17 22:25:05 +0100

    First draft of start screens and redirecting on voodoo click.

---
+++ public/js/main.js
@@ -0,0 +1,37 @@
+
+
+/*
+ * Change page when clicking on a posture voodoo.
+ */
+(function($) {
+    "use strict";
+
+    // watch page changes to assign the click event to the right posture voodoo
+    $(document).on("pagecontainerchange", function(event, data) {
+
+    	var $selector = data.toPage.find('.posture-voodoo');
+
+    	// only go on if there is such an object on the current page
+    	if($selector.length !== 0) {
+
+    		// get the target address
+		    var target = $selector.data().target;
+
+		    // check if the variable is defined (this allows non-clickable posture voodoos)
+		    if(typeof target !== 'undefined') {
+
+		    	// change the page when clicking the posture voodoo
+			    $selector.click(function() {
+			    	$.mobile.pageContainer.pagecontainer( "change", $selector.data().target );
+			    });
+
+		    }
+		}
+
+		
+	});
+
+
+    
+
+}(jQuery));
